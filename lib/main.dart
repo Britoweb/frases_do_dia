74ef46579ab7db6b7626d22a5391d260be159798
Rafael Brito <rafaelbrito.web@gmail.com>
2021-08-13 14:24:46 -0300

    Finalizada a primeira parte

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,62 @@
+
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp( MaterialApp(
+void main(){
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeStateful(),
+    home: Home(),
   ));
 }
 
-class HomeStateful extends StatefulWidget {
-  const HomeStateful({Key? key}) : super(key: key);
-
-  @override
-  _HomeStatefulState createState() => _HomeStatefulState();
-}
-
-class _HomeStatefulState extends State<HomeStateful> {
-  var _texto = "Rafael Brito 2";
-
-
-  @override
-  Widget build(BuildContext context) {
-
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text( "Frases do dia!" ),
-        backgroundColor: Colors.green,
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: (){
-                  setState(() {
-                    _texto = "Curso Flutter!";
-                  });
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.deepOrangeAccent),
-                ),
-                child: Text("Clique aqui"),
-
-            ),
-            Text("Nome: $_texto")
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    var _titulo = "Frases do dia";
-
-
     return Scaffold(
       appBar: AppBar(
-        title: Text( _titulo ),
+        title: Text("Frases do Dia"),
         backgroundColor: Colors.green,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text("Conteúdo Principal"),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.lightGreen,
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Row(
-            children: [
-              Text("Texto 1"),
-              Text("Texto 2"),
-
-            ],
-          ),
-
+      body: Container(
+        padding: EdgeInsets.all(16),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("images/logo.png"),
+            Text(
+              "Clique abaixo para gerar uma frase!",
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 17,
+                fontStyle: FontStyle.italic,
+                color: Colors.black
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Nova Frase",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+              ),
+            )
+          ],
         ),
       ),
     );
